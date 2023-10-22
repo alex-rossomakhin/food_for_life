d083a9299c804a55070f08254fd1165a6a6e32c0
Alex Ross <alexeirossomahin@yandex.ru>
2023-10-22 22:56:08 +0300

    review2.8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,8 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_stub_key')
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
-
+# DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 # ALLOWED_HOSTS = ['158.160.69.25', '127.0.0.1', 'localhost', 'taskirossomahin.ddns.net']
 
